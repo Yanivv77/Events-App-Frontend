94ef31db5e08695a87942d71f821c68d75594efb
Yanivv77 <Yanivv77@gmail.com>
2022-06-12 00:20:35 +0300

    fetch

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -2,10 +2,8 @@ const { events } = require('./data.json')
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req, res) => {
-  const evt = events.filter((ev) => ev.slug === req.query.slug)
-
   if (req.method === 'GET') {
-    res.status(200).json(evt)
+    res.status(200).json(events)
   } else {
     res.setHeader('Allow', ['GET'])
     res.status(405).json({ message: `Method ${req.method} is not allowed` })
