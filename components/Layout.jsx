f77c233d56d612f12221dd6e475e75a907028de1
Yanivv77 <Yanivv77@gmail.com>
2022-06-11 19:04:40 +0300

    showcase

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -27,7 +27,7 @@ export default function Layout({ title, keywords, description, children }) {
 }
 
 Layout.defaultProps = {
-  title: 'DJ Events | Find the hottest parties',
-  description: 'Find the latest DJ and other musical events',
+  title: ' Events | Find the hottest parties',
+  description: 'Find the latest events',
   keywords: 'music, dj, edm, events',
 }
