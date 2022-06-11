b30a22217ca460c96e9b0d2911126ab880e9ec62
Yanivv77 <Yanivv77@gmail.com>
2022-06-11 18:48:20 +0300

    module aliases

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
-import styles from '../styles/Layout.module.css'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -13,6 +17,9 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
 
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
@@ -20,7 +27,7 @@ export default function Layout({ title, keywords, description, children }) {
 }
 
 Layout.defaultProps = {
-  title: ' Events | Find the hottest parties',
-  description: 'Find the latest events',
+  title: 'DJ Events | Find the hottest parties',
+  description: 'Find the latest DJ and other musical events',
   keywords: 'music, dj, edm, events',
 }
