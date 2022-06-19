aeb6aa3f76ff7b629b29947bf7ca477b0bf48e0c
Yanivv77 <Yanivv77@gmail.com>
2022-06-20 02:04:17 +0300

    accounts

--- pages/account/register.jsx
+++ pages/account/register.jsx
@@ -15,6 +15,8 @@ export default function RegisterPage() {
 
   const { register, error } = useContext(AuthContext)
 
+  useEffect(() => error && toast.error(error))
+
   const handleSubmit = (e) => {
     e.preventDefault()
 
@@ -55,7 +57,7 @@ export default function RegisterPage() {
         </form>
 
         <p>
-          Already have an account? <Link href="/account/login">Login</Link>
+          Already have an account? <Link href="/account/login">Register</Link>
         </p>
       </div>
     </Layout>
